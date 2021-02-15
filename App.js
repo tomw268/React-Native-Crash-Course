72da3efb3ea19dba9a0588b94b2c15e39ea65a5d
tomw268 <tomw268@gmail.com>
2021-02-15 13:25:11 -0500

    added a touchable highlight

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+  Image,
+  SafeAreaView,
+} from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log('Text pressed');
@@ -8,13 +15,16 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text onPress={handlePress}>First React Native APP!!!!</Text>
-      <Image
-        source={{
-          width: 200,
-          height: 300,
-          uri: 'https://picsum.photos/200/300',
-        }}
-      />
+      <TouchableHighlight onPress={() => console.log('IMG Tapped')}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: 'https://picsum.photos/200/300',
+          }}
+        />
+      </TouchableHighlight>
+
       <StatusBar style="auto" />
     </SafeAreaView>
   );
