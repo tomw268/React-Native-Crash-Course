e3128857af91d6b4719329c9b393956d4e787ad0
tomw268 <tomw268@gmail.com>
2021-02-15 13:10:21 -0500

    added favicon

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log('Text pressed');
   return (
     <SafeAreaView style={styles.container}>
       <Text onPress={handlePress}>First React Native APP!!!!</Text>
+      <Image source={require('./assets/favicon.png')} />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -15,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'forestgreen',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
